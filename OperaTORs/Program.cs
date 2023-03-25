75a19d3f3023adabd41710ae86b214d63bf4fe15
OkruzhkoVS <OkruzhkoVS@411D-14>
2023-03-25 12:19:40 +0700

    Добавьте файлы проекта.

---
+++ OperaTORs/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OperaTORs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            RationalNums num1 = new RationalNums(a, b);
+            num1.Print();
+            Console.WriteLine();
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+            RationalNums num2 = new RationalNums(c, d);
+            num2.Print();
+            Console.WriteLine();
+            RationalNums res1 = num1 + num2;
+            Console.Write("Результат сложения: ");
+            res1.Print();
+            Console.WriteLine();
+            RationalNums res2 = num1 - num2;
+            Console.Write("Результат вычитания: ");
+            res2.Print();
+            Console.WriteLine();
+            RationalNums res3 = num1 * num2;
+            Console.Write("Результат умножения: ");
+            res3.Print();
+            Console.WriteLine();
+            RationalNums res4 = num1 / num2;
+            Console.Write("Результат деления: ");
+            res4.Print();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ OperaTORs/RationalNums.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperaTORs
+{
+    class RationalNums
+    {
+        private int chis;
+        private int znam;
+        
+        public int Chis
+        {
+            get
+            {
+                return chis;
+            }
+            set
+            {
+                chis = value;
+            }
+        }
+
+        public int Znam
+        {
+            get
+            {
+                return znam;
+            }
+            set
+            {
+                znam = value;
+            }
+        }
+        public RationalNums(int chislitel = 1, int znamenatel = 1)
+        {
+            if (znamenatel == 0)
+            {
+                throw new ArgumentException("Знаменатель не может быть нулём, ёлки-палки. Хочешь, чтобы вселенная схлопнулась?");
+            }
+            chis = chislitel;
+            znam = znamenatel;
+        }
+
+        public static RationalNums operator +(RationalNums a, RationalNums b)
+        {
+            RationalNums res = new RationalNums();
+            if (a.Znam == b.Znam)
+            {
+               res.Chis = a.Chis + b.Chis;
+               res.Znam = a.Znam;
+            }
+            else
+            {
+                res.Chis = a.Chis * b.Znam + a.Znam * b.Chis;
+                res.Znam = a.Znam * b.Znam;
+            }
+            return res;
+        }
+
+        public static RationalNums operator -(RationalNums a, RationalNums b)
+        {
+            RationalNums res = new RationalNums();
+            if (a.Znam == b.Znam)
+            {
+                res.Chis = a.Chis - b.Chis;
+                res.Znam = a.Znam;
+            }
+            else
+            {
+                res.Chis = a.Chis * b.Znam - a.Znam * b.Chis;
+                res.Znam = a.Znam * b.Znam;
+            }
+            return res;
+        }
+
+        public static RationalNums operator *(RationalNums a, RationalNums b)
+        {
+            RationalNums res = new RationalNums();
+            res.Chis = a.Chis * b.Chis;
+            res.Znam = a.Znam * b.Znam;
+            return res;
+        }
+
+        public static RationalNums operator /(RationalNums a, RationalNums b)
+        {
+            RationalNums res = new RationalNums();
+            res.Chis = a.Chis * b.Znam;
+            res.Znam = a.Znam * b.Chis;
+            return res;
+        }
+        public void Print()
+        {
+            if (chis < 0)
+            {
+                Console.WriteLine($"-({-chis}/{znam})");
+            }
+            else Console.WriteLine($"{chis}/{znam}");
+        }
+    }
+}
+
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
